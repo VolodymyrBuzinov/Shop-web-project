89928b4d2c115998e5700776bb43fded9b004daf
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-12-08 23:08:13 +0200

    finish html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
- import "./sass/main.scss";
+ 
+{require('./js/card/fetch-card.js')};
+import "./sass/main.scss";
+import cardTpl from './templates/card.hbs';
+import menuJSON from './js/card/menu';
+
+const cardRef = document.querySelector('.card__list');
+
+const menuCardList = menuJSON.reduce((acc, menuJSON) => {
+    return acc + cardTpl(menuJSON)
+}, '');
+
+cardRef.insertAdjacentHTML("beforeend", menuCardList);
+console.log(menuCardList);
+
--- webpack/configs/shared.js
+++ webpack/configs/shared.js
@@ -67,6 +67,18 @@ module.exports = env => ({
         },
       },
       {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+
+      {
         test: /\.hbs$/,
         use: 'handlebars-loader',
       },
