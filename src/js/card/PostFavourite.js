2f4782d32a8e1e6c576e167b11057660cf6e20c7
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2020-12-21 17:55:30 +0200

    add fix

--- src/js/card/PostFavourite.js
+++ src/js/card/PostFavourite.js
@@ -18,16 +18,17 @@ export default function onPost(id) {
   )
     .then(response => response.json())
     .then(result => {
-      alert({
-        title: 'Цей товар добавлено до ваших обраних',
-        delay: 1500,
-      });
       if (result.message === 'Unauthorized') {
         alert({
           title: 'Щоб добавити товар в обране будь-ласка авторизуйтесь!',
           delay: 1500,
-        });
-      }
+        }); 
+      } else {
+alert({
+        title: 'Цей товар добавлено до ваших обраних',
+        delay: 1500,
+      });
+      }      
     })
     .catch(error => console.log('error', error));
 }
